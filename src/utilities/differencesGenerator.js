101fca2c56d226eb5f3f5de6401e10a2e3981a1b
TatulArt <artum405@gmail.com>
2021-06-29 21:50:17 +0300

    gendiff update

--- src/utilities/differencesGenerator.js
+++ src/utilities/differencesGenerator.js
@@ -8,7 +8,11 @@ const compareObjects = (obj1, obj2) => {
   const result = {};
 
   const obj1Keys = Object.keys(obj1);
-  _.sortBy(obj1Keys).forEach((key) => {
+  const obj2Keys = Object.keys(obj2);
+
+  const keys = _.union(obj1Keys, obj2Keys);
+
+  _.sortBy(keys).forEach((key) => {
     if (_.hasIn(obj1, key) && _.hasIn(obj2, key) && typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
       result[`  ${key}`] = compareObjects(obj1[key], obj2[key]);
       return;
@@ -16,15 +20,13 @@ const compareObjects = (obj1, obj2) => {
 
     if (!_.hasIn(obj2, key)) {
       result[`- ${key}`] = obj1[key];
+      return;
     }
 
     if (obj2[key] === obj1[key]) {
       result[`  ${key}`] = obj1[key];
     }
-  });
 
-  const obj2Keys = Object.keys(obj2);
-  _.sortBy(obj2Keys).forEach((key) => {
     if (!_.hasIn(obj1, key)) {
       result[`+ ${key}`] = obj2[key];
       return;
