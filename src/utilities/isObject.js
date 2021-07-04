10ae4a0a8388867396dada04b24efd04912a98a9
TatulArt <artum405@gmail.com>
2021-07-04 23:06:11 +0300

    refactoring

--- src/utilities/isObject.js
+++
@@ -1,9 +0,0 @@
-const isObject = (x) => {
-  if (typeof x === 'object' && !Array.isArray(x) && x !== null && x !== undefined) {
-    return true;
-  }
-
-  return false;
-};
-
-export default isObject;
