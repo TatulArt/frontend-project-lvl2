a7874799ef00a37ba11bc6eb48070bfe5b64dc82
TatulArt <artum405@gmail.com>
2021-07-04 22:40:11 +0300

    json mode add

--- README.md
+++ README.md
@@ -4,10 +4,17 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/6ca9f0dfabe8288f2fad/maintainability)](https://codeclimate.com/github/TatulArt/frontend-project-lvl2/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/6ca9f0dfabe8288f2fad/test_coverage)](https://codeclimate.com/github/TatulArt/frontend-project-lvl2/test_coverage)
 
+Json files:
 [![asciicast](https://asciinema.org/a/QNBH17IiPmFC76eGcne3YrgAk.svg)](https://asciinema.org/a/QNBH17IiPmFC76eGcne3YrgAk)
 
+Yaml files:
 [![asciicast](https://asciinema.org/a/HlaEyDGLcyhGFdtm4UGsUARFJ.svg)](https://asciinema.org/a/HlaEyDGLcyhGFdtm4UGsUARFJ)
 
+Stylish format (default):
 [![asciicast](https://asciinema.org/a/Txin1l1KijU5876eyVBOci5WN.svg)](https://asciinema.org/a/Txin1l1KijU5876eyVBOci5WN)
 
+Plain format:
 [![asciicast](https://asciinema.org/a/VizjzvXenlMApzspY9WlKY2fU.svg)](https://asciinema.org/a/VizjzvXenlMApzspY9WlKY2fU)
+
+Json format:
+[![asciicast](https://asciinema.org/a/geaE7hZAbFOesmm5EOqe8runL.svg)](https://asciinema.org/a/geaE7hZAbFOesmm5EOqe8runL)
--- __tests__/differencesGenerator.test.js
+++ __tests__/differencesGenerator.test.js
@@ -51,9 +51,9 @@ test('emptyFilepaths', () => {
 });
 
 test('gendiffStylish', () => {
-  expect(genDiff('__fixtures__/stylish/file1.json', '__fixtures__/stylish/file2.json')).toEqual(expectedResultStylish);
-  expect(genDiff('__fixtures__/stylish/file1.yaml', '__fixtures__/stylish/file2.yaml')).toEqual(expectedResultStylish);
-  expect(genDiff('__fixtures__/stylish/file1.yaml', '__fixtures__/stylish/file2.json')).toEqual(expectedResultStylish);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectedResultStylish);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(expectedResultStylish);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.json')).toEqual(expectedResultStylish);
 });
 
 const expectedResultPlain = `Property 'common.follow' was added with value: false
@@ -69,7 +69,15 @@ Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
 
 test('gendiffPlain', () => {
-  expect(genDiff('__fixtures__/stylish/file1.json', '__fixtures__/stylish/file2.json', 'plain')).toEqual(expectedResultPlain);
-  expect(genDiff('__fixtures__/stylish/file1.yaml', '__fixtures__/stylish/file2.yaml', 'plain')).toEqual(expectedResultPlain);
-  expect(genDiff('__fixtures__/stylish/file1.yaml', '__fixtures__/stylish/file2.json', 'plain')).toEqual(expectedResultPlain);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(expectedResultPlain);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain')).toEqual(expectedResultPlain);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.json', 'plain')).toEqual(expectedResultPlain);
+});
+
+const expectedResultJson = '{"common":{"value":{"follow":{"value":false,"state":"added"},"setting1":{"value":"Value 1","state":"without changes"},"setting2":{"value":200,"state":"removed"},"setting3":{"value":[true,null],"state":"changed"},"setting4":{"value":"blah blah","state":"added"},"setting5":{"value":{"key5":"value5"},"state":"added"},"setting6":{"value":{"doge":{"value":{"wow":{"value":["","so much"],"state":"changed"}},"state":"same-name objects"},"key":{"value":"value","state":"without changes"},"ops":{"value":"vops","state":"added"}},"state":"same-name objects"}},"state":"same-name objects"},"group1":{"value":{"baz":{"value":["bas","bars"],"state":"changed"},"foo":{"value":"bar","state":"without changes"},"nest":{"value":[{"key":"value"},"str"],"state":"changed"}},"state":"same-name objects"},"group2":{"value":{"abc":12345,"deep":{"id":45}},"state":"removed"},"group3":{"value":{"deep":{"id":{"number":45}},"fee":100500},"state":"added"}}';
+
+test('gendiffJson', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toEqual(expectedResultJson);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json')).toEqual(expectedResultJson);
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.json', 'json')).toEqual(expectedResultJson);
 });
