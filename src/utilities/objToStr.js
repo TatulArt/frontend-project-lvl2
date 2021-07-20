f6f887c67e5ecd4b8a4af3c17382b5719300042b
TatulArt <artum405@gmail.com>
2021-07-20 20:01:05 +0300

    removing mutations !UNFINISHED!

--- src/utilities/objToStr.js
+++ src/utilities/objToStr.js
@@ -1,9 +1,28 @@
-const diffToStr = (diff, depthLevel = 1) => diff.map((diffElement) => {
-  /*if (Array.isArray(diffElement.value)) {
-    return `${'  '.repeat(depthLevel)}${diffElement.key}: { ${diffToStr(depthLevel.value, depthLevel + 1)} }`;
-  }*/
+import _ from 'lodash';
 
-  return `${'  '.repeat(depthLevel)}${diffElement.key}: { ${depthLevel.value} }`;
+const objectDecoder = (obj, depthLevel = 1) => {
+  const objKeys = Object.keys(obj);
+
+  return objKeys.map((key) => {
+    if (_.isObject(obj[key]) && !Array.isArray(obj[key])) {
+      return `${'  '.repeat(depthLevel)}${key}: ${objectDecoder(obj[key], depthLevel + 1)}`;
+    }
+
+    return `${'  '.repeat(depthLevel)}${key}: ${obj[key]}`;
+  });
+};
+
+const diffToStr = (diff, depthLevel = 1) => diff.map((diffElement) => {
+  if (Array.isArray(diffElement.value)) {
+    const r = diffElement.value.map((innerDiff) => diffToStr(innerDiff, depthLevel + 1));
+    return `${'  '.repeat(depthLevel)}${diffElement.key}: { ${r.join('\n')} }`;
+  }
+
+  if (_.isObject(diffElement.value) && !Array.isArray(diffElement.value)) {
+    return `${'  '.repeat(depthLevel)}${diffElement.key}: { ${objectDecoder(diffElement.value)} }`;
+  }
+
+  return `${'  '.repeat(depthLevel)}${diffElement.key}: { ${diffElement.value} }`;
 });
 
 export default diffToStr;
