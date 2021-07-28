46726ad942d808534f94642be794af7044dc3303
TatulArt <artum405@gmail.com>
2021-07-28 14:37:05 +0300

    json mode mutation-less

--- __tests__/differencesGenerator.test.js
+++ __tests__/differencesGenerator.test.js
@@ -75,7 +75,7 @@ test('gendiffPlain', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(expectedResultPlain);
 });
 
-const expectedResultJson = '{"common":{"value":{"follow":{"value":false,"state":"added"},"setting1":{"value":"Value 1","state":"unchanged"},"setting2":{"value":200,"state":"removed"},"setting3":{"value":[true,null],"state":"changed"},"setting4":{"value":"blah blah","state":"added"},"setting5":{"value":{"key5":"value5"},"state":"added"},"setting6":{"value":{"doge":{"value":{"wow":{"value":["","so much"],"state":"changed"}},"state":"same-name objects"},"key":{"value":"value","state":"unchanged"},"ops":{"value":"vops","state":"added"}},"state":"same-name objects"}},"state":"same-name objects"},"group1":{"value":{"baz":{"value":["bas","bars"],"state":"changed"},"foo":{"value":"bar","state":"unchanged"},"nest":{"value":[{"key":"value"},"str"],"state":"changed"}},"state":"same-name objects"},"group2":{"value":{"abc":12345,"deep":{"id":45}},"state":"removed"},"group3":{"value":{"deep":{"id":{"number":45}},"fee":100500},"state":"added"}}';
+const expectedResultJson = '{"common":{"follow":false,"setting1":"Value 1","setting2":200,"setting3":[true,null],"setting4":"blah blah","setting5":{"key5":"value5"},"setting6":{"doge":{"wow":["","so much"]},"key":"value","ops":"vops"}},"group1":{"baz":["bas","bars"],"foo":"bar","nest":[[{"key":"key","value":"value","state":"unchanged"}],"str"]},"group2":{"abc":12345,"deep":{"id":45}},"group3":{"deep":{"id":{"number":45}},"fee":100500}}';
 
 test('gendiffJson', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toEqual(expectedResultJson);
--- src/utilities/formatters/json.js
+++ src/utilities/formatters/json.js
@@ -1,3 +1,14 @@
-const json = (diff) => JSON.stringify(diff);
+import _ from 'lodash';
+import { addElementsToObject } from '../mutationLessUtilities.js';
+
+const getDiffObject = (diff) => diff.reduce((acc, diffElement) => {
+  if (_.isArray(diffElement.value) && diffElement.state !== 'changed') {
+    return addElementsToObject(acc, diffElement.key, getDiffObject(diffElement.value));
+  }
+
+  return addElementsToObject(acc, diffElement.key, diffElement.value);
+}, {});
+
+const json = (diff) => JSON.stringify(getDiffObject(diff));
 
 export default json;
