4531244a578522e747e24c9bbae9555e9a9244ba
TbMA <artum405@gmail.com>
2021-08-18 17:36:34 +0300

    refactoring

--- src/formatters/generatePlainDiff.js
+++ src/formatters/generatePlainDiff.js
@@ -9,21 +9,17 @@ const getValidValue = (data) => {
 };
 
 const generatePlainDiff = (diff, path = '') => diff.map((diffElement) => {
-  if (diffElement.type === 'unchanged') {
-    return;
-  }
-
-  const currentPath = `${path}${diffElement.key}`;
-
   switch (diffElement.type) {
     case 'nested':
-      return generatePlainDiff(diffElement.children, `${currentPath}.`);
+      return generatePlainDiff(diffElement.children, `${path}${diffElement.key}.`);
     case 'changed':
-      return `Property ${currentPath} was updated. From ${getValidValue(diffElement.value[0])} to ${getValidValue(diffElement.value[1])}`;
+      return `Property ${path}${diffElement.key} was updated. From ${getValidValue(diffElement.value[0])} to ${getValidValue(diffElement.value[1])}`;
     case 'added':
-      return `Property ${currentPath} was added with value: ${getValidValue(diffElement.value)}`;
+      return `Property ${path}${diffElement.key} was added with value: ${getValidValue(diffElement.value)}`;
     case 'removed':
-      return `Property ${currentPath} was removed`;
+      return `Property ${path}${diffElement.key} was removed`;
+    case 'unchanged':
+      return;
     default:
       throw new Error(`Unknown type: ${diffElement.type}`);
   }
