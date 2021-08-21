802d940674c2db26afa698765b34090668c7876a
TatulArt <artum405@gmail.com>
2021-08-21 16:45:30 +0300

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,41 +6,45 @@ const stringifyValue = (value, depth) => {
   }
 
   if (_.isObject(value)) {
+    const indent = depth * 4 - 2;
+
     const keys = Object.keys(value);
 
     const resultValue = keys.map((key) => {
       if (_.isObject(value[key]) && !Array.isArray(value[key])) {
-        return `${'  '.repeat(depth)}  ${key}: ${stringifyValue(value[key], depth + 2)}`;
+        return `${' '.repeat(indent)}  ${key}: ${stringifyValue(value[key], depth + 1)}`;
       }
 
-      return `${'  '.repeat(depth)}  ${key}: ${value[key]}`;
+      return `${' '.repeat(indent)}  ${key}: ${value[key]}`;
     }).join('\n');
 
-    return ['{', resultValue, `${'  '.repeat(depth - 1)}}`].join('\n');
+    return ['{', resultValue, `${' '.repeat(indent - 2)}}`].join('\n');
   }
 
   return String(value);
 };
 
 const renderStylish = (tree, depth = 1) => {
+  const indent = depth * 4 - 2;
+
   const resultPrototype = tree.map((node) => {
     switch (node.type) {
       case 'nested':
-        return `${'  '.repeat(depth)}  ${node.key}: ${renderStylish(node.children, depth + 2)}`;
+        return `${' '.repeat(indent)}  ${node.key}: ${renderStylish(node.children, depth + 1)}`;
       case 'changed':
-        return [`${'  '.repeat(depth)}- ${node.key}: ${stringifyValue(node.oldValue, depth + 2)}`, `${'  '.repeat(depth)}+ ${node.key}: ${stringifyValue(node.newValue, depth + 2)}`].join('\n');
+        return [`${' '.repeat(indent)}- ${node.key}: ${stringifyValue(node.oldValue, depth + 1)}`, `${' '.repeat(indent)}+ ${node.key}: ${stringifyValue(node.newValue, depth + 1)}`].join('\n');
       case 'added':
-        return `${'  '.repeat(depth)}+ ${node.key}: ${stringifyValue(node.value, depth + 2)}`;
+        return `${' '.repeat(indent)}+ ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
       case 'unchanged':
-        return `${'  '.repeat(depth)}  ${node.key}: ${stringifyValue(node.value, depth + 2)}`;
+        return `${' '.repeat(indent)}  ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
       case 'removed':
-        return `${'  '.repeat(depth)}- ${node.key}: ${stringifyValue(node.value, depth + 2)}`;
+        return `${' '.repeat(indent)}- ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
       default:
         throw new Error(`Unknown type: ${node.type}`);
     }
   }).join('\n');
 
-  const result = ['{', resultPrototype, `${'  '.repeat(depth - 1)}}`];
+  const result = ['{', resultPrototype, `${' '.repeat(indent - 2)}}`];
   return result.join('\n');
 };
 
