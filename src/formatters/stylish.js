1c02e968e3d7f95a6ffec51b81fad994bd5aeaf1
TatulArt <artum405@gmail.com>
2021-08-24 14:52:33 +0300

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,51 +1,52 @@
 import _ from 'lodash';
 
+const indent = (indentSize) => ' '.repeat(indentSize);
+
 const stringifyValue = (value, depth) => {
   if (_.isArray(value)) {
     return `[${value.join(', ')}]`;
   }
 
   if (_.isObject(value)) {
-    const indent = depth * 4 - 2;
+    const indentSize = depth * 4 - 2;
 
     const keys = Object.keys(value);
 
     const resultValue = keys.map((key) => {
       if (_.isObject(value[key]) && !Array.isArray(value[key])) {
-        return `${' '.repeat(indent)}  ${key}: ${stringifyValue(value[key], depth + 1)}`;
+        return `${indent(indentSize)}  ${key}: ${stringifyValue(value[key], depth + 1)}`;
       }
 
-      return `${' '.repeat(indent)}  ${key}: ${value[key]}`;
+      return `${indent(indentSize)}  ${key}: ${value[key]}`;
     }).join('\n');
 
-    return ['{', resultValue, `${' '.repeat(indent - 2)}}`].join('\n');
+    return ['{', resultValue, `${indent(indentSize - 2)}}`].join('\n');
   }
 
   return String(value);
 };
 
 const renderStylish = (tree, depth = 1) => {
-  const indent = depth * 4 - 2;
+  const indentSize = depth * 4 - 2;
 
-  const resultPrototype = tree.map((node) => {
+  const result = tree.map((node) => {
     switch (node.type) {
       case 'nested':
-        return `${' '.repeat(indent)}  ${node.key}: ${renderStylish(node.children, depth + 1)}`;
+        return `${indent(indentSize)}  ${node.key}: ${renderStylish(node.children, depth + 1)}`;
       case 'changed':
-        return [`${' '.repeat(indent)}- ${node.key}: ${stringifyValue(node.oldValue, depth + 1)}`, `${' '.repeat(indent)}+ ${node.key}: ${stringifyValue(node.newValue, depth + 1)}`].join('\n');
+        return [`${indent(indentSize)}- ${node.key}: ${stringifyValue(node.oldValue, depth + 1)}`, `${indent(indentSize)}+ ${node.key}: ${stringifyValue(node.newValue, depth + 1)}`].join('\n');
       case 'added':
-        return `${' '.repeat(indent)}+ ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
+        return `${indent(indentSize)}+ ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
       case 'unchanged':
-        return `${' '.repeat(indent)}  ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
+        return `${indent(indentSize)}  ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
       case 'removed':
-        return `${' '.repeat(indent)}- ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
+        return `${indent(indentSize)}- ${node.key}: ${stringifyValue(node.value, depth + 1)}`;
       default:
         throw new Error(`Unknown type: ${node.type}`);
     }
   }).join('\n');
 
-  const result = ['{', resultPrototype, `${' '.repeat(indent - 2)}}`];
-  return result.join('\n');
+  return ['{', result, `${indent(indentSize - 2)}}`].join('\n');
 };
 
 export default (tree) => renderStylish(tree);
